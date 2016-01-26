19cf4b7df111360beb4adc724ff617176c324202
Romain Paillard <romain.paillard@gmail.com>
2016-01-26 20:06:26 +0100

    added date to poem

---
+++ db/migrate/20160126190416_add_date_to_poems.rb
@@ -0,0 +1,7 @@
+class AddDateToPoems < ActiveRecord::Migration
+  def change
+    def change
+      add_column :poems, :date, :datetime
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126185849) do
+ActiveRecord::Schema.define(version: 20160126190416) do
 
   create_table "poems", force: :cascade do |t|
     t.string   "title"
